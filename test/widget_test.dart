f26496b009c9797bc3f1de9eba50d89652df8d53
François Grèzes <grezesfrancois@gmail.com>
2019-09-15 12:36:24 +0200

    fixed issue sounds index not written in config file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:ahhhhhh/storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,7 @@ import 'package:ahhhhhh/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App());
+    await tester.pumpWidget(App(storage: Storage(),));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
